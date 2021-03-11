b80dcf09abb1627ed13b378a5039a77f55699dbb
Nicholas Mendez <snickdx@gmail.com>
2021-03-11 20:46:49 +0000

    added post route

--- App/controllers/user.py
+++ App/controllers/user.py
@@ -1,7 +1,21 @@
 from flask import redirect, render_template, request, session, url_for
 
-from App.models import ( User )
+# Can import models
+from App.models import ( User, db )
 
-def create_user(firstname, lastname, uwi_id, email, gender, dob):
-    # newuser = use()
-    return 'new user'
+def get_users_json():
+    users = User.query.all()
+    if not users:
+        return jsonify([])
+    json = [user.toDict() for user in users]
+    return json
+
+def create_user(first_name, last_name):
+    newuser = Use(first_name=first_name, last_name=last_name)
+    db.session.add(newuser)
+    db.session.commit()
+    return True
+
+def get_users():
+    return User.query.all()
+
