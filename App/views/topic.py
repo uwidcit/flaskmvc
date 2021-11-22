09f50ff856d555be286032688ac563a69287912c
Raydon Davis <ray_5610@hotmail.com>
2021-11-22 05:54:09 +0000

    creation of a view and controller for 'topic'

---
+++ App/views/topic.py
@@ -0,0 +1,48 @@
+from flask import Blueprint, jsonify, request
+from flask_jwt import jwt_required
+import json
+
+topic_views = Blueprint('topic_views', __name__, template_folder='../templates')
+
+from App.controllers import (
+create_topic,
+create_subTopic,
+get_topic_by_id,
+delete_subTopic_by_id,
+)
+
+# create Topic
+@order_views.route('/create-topic', methods=["POST"])
+@jwt_required()
+def create_topic():
+    #topic_id
+    topic_name = request.json.get('topic_name')
+    topic_description = request.json.get('topic_description')
+    #date_timestamp
+    newTopic = create_topic(topic_id, topic_name, topic_description,date_timestamp)
+    return jsonify(Topic.toDict())
+
+# create SubTopic
+@order_views.route('/create-topic', methods=["POST"])
+@jwt_required()
+def create_Subtopic():
+    #Maintopic_id
+    Subtopic_name = request.json.get('Subtopic_name')
+    Subtopic_description = request.json.get('Subtopic_description')
+    #date_timestamp
+    newTopic = create_subTopic(Maintopic_id, Subtopic_id, Subtopic_name,Subtopic_description,date_timestamp)
+    return jsonify(SubTopic.toDict())
+
+# get specific Topic by ID
+@order_views.route('/topic_id', methods=["GET"])
+def get_topic_id():
+    Topic_id = request.args.get("id")
+    order = get_topic_by_id(Topic_id)
+    return jsonify(Topic.toDict())
+
+#Deletes topic from database and returns if it was successful
+@customer_views.route('/delete-subTopic', methods=["DELETE"])
+def delete_subTopic():
+    subTopic_id = request.args.get("sub_topic_id")
+    deleted = delete_subTopic_by_id(subTopic_id)
+    return jsonify({"deleted" : deleted})
