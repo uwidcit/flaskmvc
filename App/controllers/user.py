ee1cbec97417479374071f9f625feb5a276be961
Whitney Shoy <alyssa-alexandra@hotmail.com>
2021-11-22 05:25:48 +0000

    Updated Controller

--- App/controllers/user.py
+++ App/controllers/user.py
@@ -1,6 +1,7 @@
 from flask import redirect, render_template, request, url_for, jsonify
 
 from App.models import ( User, db )
+from App.models import user
 
 def create_user(firstname, lastname, uwi_id, email, gender, dob):
     # newuser = use()
@@ -31,3 +32,22 @@ def get_user_by_fname(first_name):
 
 def get_all_users():
     return  User.query.all()
+
+# REGISTER A NORMAL USER
+def register():
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        email = request.form['email']
+        password = request.form['password']
+        publicUser = User(first_name=first_name, last_name=last_name, email=email, password=password)
+        db.session.add(publicUser)
+        db.session.commit()
+    
+# REGISTER ADMIN USER
+def register_admin(firstname, lastname, email, password):
+        adminUser = User(first_name=firstname, last_name=lastname, email=email,password=password)
+        adminUser.set_password(password)
+        db.session.add(adminUser)
+        db.session.commit()
+        print("Administrator Successfully Created!!!")
+        return adminUser
