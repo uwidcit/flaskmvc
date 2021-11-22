9bc80fc821c297fa746385da6e3e909d19a16581
Whitney Shoy <alyssa-alexandra@hotmail.com>
2021-11-22 03:47:25 +0000

    Main Update

--- App/main.py
+++ App/main.py
@@ -15,11 +15,12 @@ from flask_uploads import (
 
 from App.views import (
     api_views,
-    user_views
+    user_views,
+    user_registration
 )
 
 #place all views here
-views = [api_views, user_views]
+views = [api_views, user_views , user_registration]
 
 def add_views(app, views):
     for view in views:
