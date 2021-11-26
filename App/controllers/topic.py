625215c83bfbfc7ea9d275e1ea52cf148f9b34c0
Raydon Davis <ray_5610@hotmail.com>
2021-11-26 01:38:14 +0000

    update topic controller

--- App/controllers/topic.py
+++ App/controllers/topic.py
@@ -1,46 +1,51 @@
 # from App.models import ( Topic, db )
 
 
-# #create new topic
-# def create_topic(topic_id, topic_name, topic_description, date_timestamp):
-#     newTopic = Topic(topic_id = topic.id, topic_name = topic_name, topic_description = topic_description, date_timestamp = date_timestamp)
-#     print("Successfully Created")
-#     db.session.add(newTopic)
-#     db.session.commit()
-#     return newTopic
+# #create new topic, ID and time stamp will be done in DB
+def create_topic( topic_name, topic_description):
+     newTopic = Topic(topic_name = topic_name, topic_description = topic_description)
+     print("Successfully Created")
+     db.session.add(newTopic)
+     db.session.commit()
+     return newTopic
 
-# #create new subtopic
-# def create_subTopic(Maintopic_id, Subtopic_id , Subtopic_name, Subtopic_description, date_timestamp):
-#     newSubTopic = create_subTopic(Maintopic_id = Maintopic_id,Subtopic_id=Subtopic_id, Subtopic_name = Subtopic_name, Subtopic_description = Subtopic_description, date_timestamp = date_timestamp)
-#     print("Successfully Created")
-#     db.session.add(newSubTopic)
-#     db.session.commit()
-#     return newSubTopic
+#update a topic that already exist
+def update_topic(topic_id, topic_name , topic_description):
+    print("getting Topic")
+    topic_update = Topic.query.filter(topic.id == topic_id).first()
+    topic_update.topic_name = topic_name
+    topic_update.topic_description = topic_description
+    db.session.add(topic_update)
+    return topic_update
 
-# # get topic by id
-# def get_topic_by_id(topic_id):
-#     print("getting Topic")
-#     order = Topic.query.filter(topic.id == topic_id).first() 
-#     return topic
+# delete Topic by id
+def delete_topic_by_id(topic_id):
+     print("deleting SubTopic")
+     topic = Topic.query.filter(topic.id ==topic_id).first()
+     if topic:
+         db.session.delete(topic)
+         db.session.commit()
+         return True
+     return False
 
-# #  get_topic_by_term
-# def get_topic_by_term(term):
-#     list_of_topic = []
-#     topic = Topic.query.filter(
-#         Topic.topic_name.contains(term)
-#     | Topic.topic_description.contains(term)
-#     | Topic.Subtopic_name.contains(term))
-#     if topic:
-#         list_of_topic = [o.toDict() for o in topic]
-#     return list_of_topic
+# get topic by id
+def get_topic_by_id(topic_id):
+     print("getting Topic")
+     order = Topic.query.filter(topic.id == topic_id).first() 
+     return topic
 
 
-# # deleting subTopic by id
-# def delete_subTopic_by_id(Subtopic_id):
-#     print("deleting SubTopic")
-#     subtopic = User.query.filter(Subtopic_id == Subtopic_id).first()
-#     if sustomer:
-#         db.session.delete(subtopic)
-#         db.session.commit()
-#         return True
-#     return False
+# get list of ALL topics
+def get_topics():
+    print('get all orders')
+    topics = Topic.query.all()
+    list_of_topics = []
+    if orders:
+        list_of_topics = [o.toDict() for o in topics]
+    return list_of_topics
+
+ # get topic by id
+def get_topic_by_level(topiclvl_id):
+     print("getting Topic")
+     topic_lvl = Topic.query.filter(topic.level == topiclvl_id).first() 
+     return topic_lvl
