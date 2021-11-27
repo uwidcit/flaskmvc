ed6234416da8fba83f023f0f44c692915be3e506
Whitney Shoy <alyssa-alexandra@hotmail.com>
2021-11-27 18:05:47 +0000

    Model

---
+++ App/models/reply.py
@@ -0,0 +1,23 @@
+from flask_sqlalchemy import SQLAlchemy
+
+# init db
+db = SQLAlchemy()
+
+
+class Reply(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    originalPostId = db.Column(db.Integer, db.ForeignKey('user.id'))
+    
+
+    def __init__(self, originalPostId):
+        self.originalPostId = originalPostId
+        
+
+    def __repr__(self):
+        return f"{self.originalPostId}"
+
+
+    def toDict(self):
+        return {
+            "originalPostId": self.originalPostId
+        }
