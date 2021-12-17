ea25a377f9f5a3d20ba1a6d65b71140b4b000e06
rickydindial23 <60444200+rickydindial23@users.noreply.github.com>
2021-12-17 17:07:37 -0400

    Update api.py

--- App/views/api.py
+++ App/views/api.py
@@ -4,4 +4,4 @@ api_views = Blueprint('api_views', __name__, template_folder='../templates')
 
 @api_views.route('/', methods=['GET'])
 def get_api_docs():
-    return render_template('index.html')
+    return render_template('homepage.html')
