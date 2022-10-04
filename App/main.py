5572cbd4c61cbb4edc465c79e7c8f33a64ab5432
Nicholas Mendez <snickdx@gmail.com>
2022-10-04 19:03:59 -0400

    timedelta fix patch

--- App/main.py
+++ App/main.py
@@ -33,15 +33,18 @@ def add_views(app, views):
 
 def loadConfig(app, config):
     app.config['ENV'] = os.environ.get('ENV', 'DEVELOPMENT')
+    delta
     if app.config['ENV'] == "DEVELOPMENT":
         app.config.from_object('App.config')
+        delta = app.config['JWT_EXPIRATION_DELTA']
     else:
         app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
         app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-        app.config['JWT_EXPIRATION_DELTA'] =  timedelta(days=int(os.environ.get('JWT_EXPIRATION_DELTA')))
         app.config['DEBUG'] = os.environ.get('ENV').upper() != 'PRODUCTION'
         app.config['ENV'] = os.environ.get('ENV')
+        delta = app.config['JWT_EXPIRATION_DELTA']
         
+    app.config['JWT_EXPIRATION_DELTA'] = timedelta(days=int(delta))    
     for key, value in config.items():
         app.config[key] = config[key]
 
