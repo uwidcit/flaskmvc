62cbfa66406675004d5c4f7729b83ede9f3de87a
Nicholas Mendez <snickdx@gmail.com>
2022-10-05 18:26:25 +0000

    updated jwt expiration in prod

--- App/main.py
+++ App/main.py
@@ -42,7 +42,7 @@ def loadConfig(app, config):
         app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
         app.config['DEBUG'] = os.environ.get('ENV').upper() != 'PRODUCTION'
         app.config['ENV'] = os.environ.get('ENV')
-        delta = app.config['JWT_EXPIRATION_DELTA']
+        delta = os.environ.get('JWT_EXPIRATION_DELTA', 7)
         
     app.config['JWT_EXPIRATION_DELTA'] = timedelta(days=int(delta))
         
