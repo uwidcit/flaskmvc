e285b713ffe0229caa50f339b9d0e9d13a0785f2
ShivsTKD <shivanmaharaj.sm@gmail.com>
2023-02-04 18:14:33 +0000

    added more models

--- App/models/publication.py
+++ App/models/publication.py
@@ -4,4 +4,4 @@ class Publication(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120), unique=True, nullable=False)
     abstract = db.Column(db.String(500), nullable=False)
-    
+    library_id = db.Column(db.Integer, ForeignKey("Library.id"), nullable=True)
