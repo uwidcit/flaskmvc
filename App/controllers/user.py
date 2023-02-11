1ad10fa7984b51aab301889053e9192d113d84ea
satyaandm <satyaandm@gmail.com>
2023-02-10 21:46:06 -0400

    user model and controller

--- App/controllers/user.py
+++ App/controllers/user.py
@@ -1,33 +1,34 @@
-from App.models import User
+from App.models.user import User
 from App.database import db
 
-def create_user(username, password):
-    newuser = User(username=username, password=password)
-    db.session.add(newuser)
+
+def create_user(username, password, access):
+    new_user = User(username=username, password=password, access=access)
+    db.session.add(new_user)
     db.session.commit()
-    return newuser
+    return new_user
+
 
 def get_user_by_username(username):
     return User.query.filter_by(username=username).first()
 
-def get_user(id):
+
+def get_user_by_id(id):
     return User.query.get(id)
+
 
 def get_all_users():
     return User.query.all()
 
+
 def get_all_users_json():
-    users = User.query.all()
-    if not users:
-        return []
-    users = [user.toJSON() for user in users]
-    return users
+    return [user.to_json() for user in get_all_users()]
+
 
 def update_user(id, username):
-    user = get_user(id)
+    user = get_user_by_id(id)
     if user:
         user.username = username
         db.session.add(user)
         return db.session.commit()
     return None
-    
--- App/models/user.py
+++ App/models/user.py
@@ -1,71 +1,49 @@
 from werkzeug.security import check_password_hash, generate_password_hash
 from App.database import db
 
-ACCESS = {
-    'user': 1,
-    'farmer': 2,
-    'admin': 3
-}
+ACCESS = {"user": 1, "farmer": 2, "admin": 3}
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, nullable=False)
     password = db.Column(db.String(120), nullable=False)
-    email = db.Column(db.String(120), nullable=False)
-    phone = db.Column(db.String(120), nullable=False)
-    address = db.Column(db.String(120), nullable=False)
-    currency = db.Column(db.String(120), nullable=False)
-    units = db.Column(db.String(10), nullable=False)
-    avatar = db.Column(db.String(120), nullable=False)
-    access = db.Column(db.Integer, nullable=False, default=ACCESS['user'])
+    access = db.Column(db.Integer, nullable=False)
+    # email = db.Column(db.String(120), nullable=False)
+    # phone = db.Column(db.String(120), nullable=False)
+    # address = db.Column(db.String(120), nullable=False)
+    # currency = db.Column(db.String(120), nullable=False)
+    # units = db.Column(db.String(10), nullable=False)
+    # avatar = db.Column(db.String(120), nullable=False)
 
-    def __init__(self, username, password, email, phone, address, currency, units, avatar, access=ACCESS['user']):
+    def __init__(self, username, password, access="user"):
         self.username = username
         self.set_password(password)
-        self.email = email
-        self.phone = phone
-        self.address = address
-        self.currency = currency
-        self.units = units
-        self.avatar = avatar
-        self.access = access
+        # self.email = email
+        # self.phone = phone
+        # self.address = address
+        # self.currency = currency
+        # self.units = units
+        # self.avatar = avatar
+        self.access = ACCESS[access]
 
     def to_json(self):
-        return{
-            'id': self.id,
-            'username': self.username,
-            'email': self.email,
-            'phone': self.phone,
-            'address': self.address,
-            'currency': self.currency,
-            'units': self.units,
-            'avatar': self.avatar,
-            'access': self.access
+        return {
+            "id": self.id,
+            "username": self.username,
+            # "email": self.email,
+            # 'phone': self.phone,
+            # 'address': self.address,
+            # 'currency': self.currency,
+            # 'units': self.units,
+            # 'avatar': self.avatar,
+            "access": self.access,
         }
 
     def set_password(self, password):
         """Create hashed password."""
-        self.password = generate_password_hash(password, method='sha256')
-    
+        self.password = generate_password_hash(password, method="sha256")
+
     def check_password(self, password):
         """Check hashed password."""
         return check_password_hash(self.password, password)
-
-    def set_email(self, email):
-        self.email = email
-
-    def set_phone(self, phone):
-        self.phone = phone
-
-    def set_address(self, address):
-        self.address = address
-
-    def set_currency(self, currency):
-        self.currency = currency
-
-    def set_units(self, units):
-        self.units = units
-
-    def set_avatar(self, avatar):
-        self.avatar = avatar
