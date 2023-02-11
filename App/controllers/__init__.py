86885374326ae906fb59b39b7e7a329c39ac7d30
satyaandm <satyaandm@gmail.com>
2023-02-10 21:46:55 -0400

    model and controller imports

--- App/controllers/__init__.py
+++ App/controllers/__init__.py
@@ -1,2 +1,4 @@
 from .user import *
 from .auth import *
+from .product import *
+from .review import *
--- App/models/__init__.py
+++ App/models/__init__.py
@@ -1,1 +1,5 @@
 from .user import *
+
+# from .order import *
+from .product import *
+from .review import *
