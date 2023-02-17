1b3d1c8f866e37f2dbb591a0c3c21db81b2bdcda
satyaandm <satyaandm@gmail.com>
2023-02-17 08:48:37 -0400

    model and controller for replies to product reviews

---
+++ App/controllers/reply.py
@@ -0,0 +1,52 @@
+from App.models.reply import Reply
+from App.database import db
+
+
+def create_reply(review_id, user_id, body, timestamp):
+    new_reply = Reply(review_id=review_id, user_id=user_id, body=body, timestamp=timestamp)
+    db.session.add(new_reply)
+    db.session.commit()
+    return new_reply
+
+
+def get_all_replies_by_review_id(review_id):
+    return Reply.query.filter_by(review_id=review_id).all()
+
+
+def get_all_replies_by_review_id_json(review_id):
+    return [reply.to_json() for reply in get_all_replies_by_review_id(review_id)]
+
+
+def get_reply_by_id(reply_id):
+    return Reply.query.get(reply_id)
+
+
+def get_reply_by_id_json(reply_id):
+    return get_reply_by_id(reply_id).to_json()
+
+
+def get_replies_by_user_id(user_id):
+    return Reply.query.filter_by(user_id=user_id).all()
+
+
+def get_replies_by_user_id_json(user_id):
+    return [reply.to_json() for reply in get_replies_by_user_id(user_id)]
+
+
+def update_reply(reply_id, body):
+    reply = get_reply_by_id(reply_id)
+    if reply:
+        reply.body = body
+        db.session.add(reply)
+        return db.session.commit()
+    return None
+
+
+def delete_reply(reply_id):
+    reply = get_reply_by_id(reply_id)
+    if reply:
+        db.session.delete(reply)
+        return db.session.commit()
+    return None
+
+
---
+++ App/models/reply.py
@@ -0,0 +1,24 @@
+from App.database import db
+
+
+class Reply(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    review_id = db.Column(db.Integer, db.ForeignKey("review.id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    body = db.Column(db.String(1024), nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False)
+
+    def __init__(self, review_id, user_id, body, timestamp):
+        self.review_id = review_id
+        self.user_id = user_id
+        self.body = body
+        self.timestamp = timestamp
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "review_id": self.review_id,
+            "user_id": self.user_id,
+            "body": self.body,
+            "timestamp": self.timestamp,
+        }
