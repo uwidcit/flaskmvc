14c162e0dc16b42372e63cb5fbbe925aa31bc355
Nicholas Mendez <snickdx@gmail.com>
2023-03-07 04:10:39 +0000

    added some comments

--- App/views/__init__.py
+++ App/views/__init__.py
@@ -1,6 +1,8 @@
-# blue prints are imported explicitly instead of using *
+# blue prints are imported 
+# explicitly instead of using *
 from .user import user_views
 from .index import index_views
 
 
-views = [user_views, index_views] # blueprints must be added to this list
+views = [user_views, index_views] 
+# blueprints must be added to this list
