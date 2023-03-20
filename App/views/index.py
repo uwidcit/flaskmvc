62c47dcd3709d7944755c3b824bc155aa01d9648
Nicholas Mendez <snickdx@gmail.com>
2023-03-20 17:46:19 +0000

    added init route

--- App/views/index.py
+++ App/views/index.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, redirect, render_template, request, send_from_directory, jsonify
+from models import db
 
 index_views = Blueprint('index_views', __name__, template_folder='../templates')
 
@@ -6,6 +7,13 @@ index_views = Blueprint('index_views', __name__, template_folder='../templates')
 def index_page():
     return render_template('index.html')
 
-@index_views.route('/', methods=['GET'])
+@index_views.route('/init', methods=['GET'])
+def init():
+    db.drop_all()
+    db.create_all()
+    create_user('bob', 'bobpass')
+    return jsonfiy(message='db initialized!')
+
+@index_views.route('/health', methods=['GET'])
 def health_check():
     return jsonify({'status':'healthy'})
