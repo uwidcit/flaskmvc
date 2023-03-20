2b54208f6f73013cf513b4c290b7541ac7b126c5
Nicholas Mendez <snickdx@gmail.com>
2023-03-20 19:36:42 +0000

    fixed code error

--- App/views/index.py
+++ App/views/index.py
@@ -13,7 +13,7 @@ def init():
     db.drop_all()
     db.create_all()
     create_user('bob', 'bobpass')
-    return jsonfiy(message='db initialized!')
+    return jsonify(message='db initialized!')
 
 @index_views.route('/health', methods=['GET'])
 def health_check():
