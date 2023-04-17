2d3eeaa4707b7f5eabb412c68ae169803cba1db6
raydonbabs <78103414+raydonbabs@users.noreply.github.com>
2023-04-17 17:57:17 -0400

    Create users for tests commands

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,7 @@ from flask.cli import with_appcontext, AppGroup
 
 from App.database import db, get_migrate
 from App.main import create_app
-from App.controllers import ( create_user, get_all_users_json, get_all_users )
+from App.controllers import ( create_user, get_all_users_json, get_all_users, get_user_by_username )
 
 # This commands file allow you to create convenient CLI commands for testing controllers
 
@@ -25,7 +25,7 @@ User Commands
 
 # Commands can be organized using groups
 
-# create a group, it would be the first argument of the comand
+# create a group, it would be the first argument of the command
 # eg : flask user <command>
 user_cli = AppGroup('user', help='User object commands') 
 
@@ -67,3 +67,25 @@ def user_tests_command(type):
     
 
 app.cli.add_command(test)
+
+#creates users for tests
+@app.cli.command("create-users")
+@click.argument("number", default=4)
+def create_users_command(number):
+    for i in range(1, number + 1):
+        user = get_user_by_username(f"rob{i}")
+        if not user:
+            create_user(f"rob{i}", f"rob{i}pass")
+            print(f"rob{i} created!")
+        else:
+            print(f"rob{i} already exists")
+
+@app.cli.command("create-user")
+@click.argument("number", default="1")
+def create_user_command(number):
+    user = get_user_by_username(f"bob{number}")
+    if not user:
+        create_user(f"bob{number}", "bobpass")
+        print(f"bob{number} created!")
+    else:
+        print(f"bob{number} already exists")
