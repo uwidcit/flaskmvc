44a65c2ca9545ff8ecbd80dba30e2e35e5ad96e7
raydonbabs <78103414+raydonbabs@users.noreply.github.com>
2023-04-19 19:51:04 -0400

    Update controllers

--- App/controllers/user.py
+++ App/controllers/user.py
@@ -1,49 +1,75 @@
 from App.models import User
 from App.database import db
 
-def create_user(username, password):
-    user = get_user_by_username(username)
-    if not user:
-        new_user = User(username=username, password=password)
-        db.session.add(new_user)
-        db.session.commit()
-        return new_user
-    return None
+def create_user(username, email, password, access ="user"):
+    user1 = get_user_by_email(email)
+    user2 = get_user_by_username(username)
+    if user1 or user2:
+        return None
+    new_user = User(username = username, email = email, password = password, access = access)
+    db.session.add(new_user)
+    db.session.commit()
+    return new_user
 
-def get_user_by_username(username):
-    user = User.query.filter_by(username=username).first()
-    return user
+def create_admin(username, email, password, access="admin"):
+    return create_user(username, email, password, access)
 
-def get_user(id):
-    return User.query.get(id)
-
-def get_all_users():
-    return User.query.all()
-
-def get_user_json(id):
-    user = get_user(id)
+def update_access(id, access):
+    user = get_user_by_id(id)
     if user:
-        return user.to_json()
-    return None  
-
-def get_all_users_json():
-    users = User.query.all()
-    if not users:
-        return []
-    users = [user.to_json() for user in users]
-    return users
-
-def update_user(id, username):
-    user = get_user(id)
-    if user:
-        user.username = username
+        user.access = access
         db.session.add(user)
         db.session.commit()
         return user
     return None
 
+def get_user_by_email(email):
+    return User.query.filter_by(email=email).first()
+
+def get_user_by_username(username):
+    user = User.query.filter_by(username=username).first()
+    if user:
+        return user
+    return None
+
+def get_user_by_id(id):
+    return User.query.get(id)
+
+def get_all_users():
+    return User.query.all()
+
+def get_all_users_json():
+    return [user.to_json() for user in get_all_users()]
+
+def get_all_admins():
+    return User.query.filter_by(access="admin").all()
+
+def get_all_admins_json():
+    return [admin.to_json() for admin in get_all_admins()]
+  
+def update_user(id, username="", email="", password=""):
+    user = get_user_by_id(id)
+    if user:
+        if username:
+            user.username = username
+        if email:
+            user.email = email
+        if password:
+            user.set_password(password)
+        db.session.add(user)
+        db.session.commit()
+        return user
+    return None
+
+def is_admin(user):
+    return user.get_access() == "admin"
+
+def check_password(user, password):
+    return user.check_password(password)
+
+
 def delete_user(id):
-    user = get_user(id)
+    user = get_user_by_id(id)
     if user:
         db.session.delete(user)
         db.session.commit()
