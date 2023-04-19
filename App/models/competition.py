8975cac29baac65c5582a3eff596b21afac15385
raydonbabs <78103414+raydonbabs@users.noreply.github.com>
2023-04-19 14:40:46 -0400

    Update get json

--- App/models/competition.py
+++ App/models/competition.py
@@ -6,9 +6,7 @@ from sqlalchemy.sql.expression import func
 db = SQLAlchemy()
 
 class Competition(db.Model):
-    compCode = db.Column(db.Integer, primary_key=True)
-    #create a new realationship with the Team table
-    #teamId = db.Column(db.Integer, db.ForeignKey('team.id'), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
     adminId = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     compName = db.Column(db.String(120), nullable=False)
     startDate = db.Column(db.Date, nullable=False)
@@ -20,11 +18,12 @@ class Competition(db.Model):
         self.compName = compName
         self.startDate = startDate
         self.endDate = endDate
-# add in others
+        
     def get_json(self):
         return{
-            "compCode": self.compCode,
+            "id": self.id,
             "compName": self.compName,
             "startDate": self.startDate,
             "endDate": self.endDate,
+            "teams": [team.to_json() for team in self.teams],
         }
