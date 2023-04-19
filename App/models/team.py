cdc4eab773b6b3571211dc1670664eb5505aead5
raydonbabs <78103414+raydonbabs@users.noreply.github.com>
2023-04-19 15:14:00 -0400

    Update models

--- App/models/team.py
+++ App/models/team.py
@@ -7,22 +7,18 @@ db = SQLAlchemy()
 
 class Team(db.Model):
     id = db.Column(db.Integer, primary_key=True) 
-    competitionId = db.Column(db.Integer, db.ForeignKey("compCode"),nullable=False)
     teamName = db.Column(db.String(120), nullable=False)
     score = db.Column(db.String(120), nullable = False)
     members = db.relationship('Member', backref='team', lazy=True, cascade="all, delete-orphan")
     
-    def __init__(self, competitionId, teamName, score):
-        self.competitionId = competitionId
+    def __init__(self, teamName, score):
         self.teamName = teamName
         self.score = score
-    
+
     def to_json(self):
         return{
             "id": self.id,
-            "competitionId": self.competitionId,
             "teamName": self.teamName,
             "score": self.score,
-            "members": [member.to_json() for member in self.members],
-            
+            "members": [member.to_json() for member in self.members],   
         }
