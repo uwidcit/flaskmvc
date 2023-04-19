a88e45fc7eb3637d0ac2663daab1bc546161e126
raydonbabs <78103414+raydonbabs@users.noreply.github.com>
2023-04-18 22:59:12 -0400

    Add methods

---
+++ App/controllers/member.py
@@ -0,0 +1,31 @@
+from App.database import db 
+from App.models.member import Member
+
+def create_member(memberName):
+    member = Member(memberName)
+    db.session.add(member)
+    db.session.commit()
+    return member
+
+def get_member(memberId):
+    return Member.query.get(memberId)
+	
+def get_all_members():
+    return Member.query.all()
+		
+def update_member(memberId, memberName):
+    member = get_member(memberId)
+    if member: 
+        member.memberName = memberName
+        db.session.add(member)
+        db.session.commit()
+        return member
+    return None
+
+def delete_member(memberId):
+    member = get_member(memberId)
+    if member:
+        db.session.delete(member)
+        db.session.commit()
+        return True
+    return False
