de71e8117065dab6b73ab2acc8913c5cd86f30de
raydonbabs <78103414+raydonbabs@users.noreply.github.com>
2023-04-20 16:07:49 -0400

    Update model

--- App/controllers/competition.py
+++ App/controllers/competition.py
@@ -1,49 +1,49 @@
 from App.database import db
-from App.models import competition
+from App.models import Competition
 
 #Function to get competition by id
 def get_competition_by_id(id):
-    competition = competition.query.get(id)
+    competition = Competition.query.get(id)
     return competition
 
 #Function to get competition in json format
 def get_competition_by_id_json(id):
-    competition = competition.query.get(id)
+    competition = Competition.query.get(id)
     return competition.to_json()
 
 #Function to get all competitions
-def get_all_competition():
-    competition = competition.query.all()
+def get_all_competitions():
+    competition = Competition.query.all()
     return competition
 
 #Function to get all competitions in json format
-def get_all_competition_json():
-    competition = competition.query.all()
+def get_all_competitions_json():
+    competition = Competition.query.all()
     return competition.to_json()
 
 #Function to get competition by name
-def get_competition_by_Name(name):
-    competition = competition.query.filter_by(name = name).all()
+def get_competition_by_name(name):
+    competition = Competition.query.filter_by(name = name).first()
     return competition
 
 #Function to get competition by name in json format
-def get_competition_by_name(name):
-    competition = competition.query.filter_by(name = name).all()
-    return [competition.to_json() for competition in competition]
+def get_competition_by_name_json(name):
+    competitions = Competition.query.filter_by(name = name).first()
+    return [competition.to_json() for competition in competitions]
 
 #Function to get Start Date
 def get_start_date(id):
-    competition = competition.query.get(id)
+    competition = Competition.query.get(id)
     return competition.startDate
 
 #Function to get End Date
 def get_end_date(id):
-    competition = competition.query.get(id)
+    competition = Competition.query.get(id)
     return competition.endDate
 
 #Function to delete competition
 def delete_competition(id):
-    competition = competition.query.get(id)
+    competition = Competition.query.get(id)
     if competition:
         db.session.delete(competition)
         db.session.commit()
