18995bc2536f5d8164ea35d82d09d5e51664eb09
P-jay76 <prestonajoseph@gmail.com>
2023-04-21 15:25:45 +0000

    new regular model

--- App/models/user.py
+++ App/models/user.py
@@ -4,17 +4,20 @@ from App.database import db
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username =  db.Column(db.String, nullable=False, unique=True)
+    username =  db.Column(db.String(80), nullable=False, unique=True)
+    email = db.Column(db.string(90), nullable=False, unique=True)
     password = db.Column(db.String(120), nullable=False)
 
     def __init__(self, username, password):
         self.username = username
+        self.email = email
         self.set_password(password)
 
     def get_json(self):
         return{
             'id': self.id,
             'username': self.username
+            'email': self.email
         }
 
     def set_password(self, password):
@@ -25,3 +28,17 @@ class User(db.Model, UserMixin):
         """Check hashed password."""
         return check_password_hash(self.password, password)
 
+class RegularUser(User):
+  __tablename__='regular_user'
+  competitions = db.relationship('Competition', backref='user', lazy=True) # sets up a relationship to competitions which references User
+
+  def get_json(self):
+    return {
+      "id": self.id,
+      "username": self.username,
+      "email": self.email,
+      "role": 'regular user'
+    }
+
+  def __repr__(self):
+    return f'<RegularUser {self.id} : {self.username} - {self.email}>' 
