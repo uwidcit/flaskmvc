4c464e0d7a1ba619fce123c8cc4a058f85302bbd
raydonbabs <78103414+raydonbabs@users.noreply.github.com>
2023-04-21 18:19:04 -0400

    Update imports

--- App/views/index.py
+++ App/views/index.py
@@ -1,6 +1,12 @@
-from flask import Blueprint, redirect, render_template, request, send_from_directory, jsonify
-from App.models import db
-from App.controllers import create_user
+# from flask import Blueprint, redirect, render_template, request, send_from_directory, jsonify
+# from App.models import db
+# from App.controllers import create_user
+
+from flask import (
+    Blueprint,
+    render_template,
+    jsonify,
+)
 
 index_views = Blueprint('index_views', __name__, template_folder='../templates')
 
