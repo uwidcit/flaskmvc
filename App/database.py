83312a199b3d51b461f54486e06e4fd658b33b8d
raydonbabs <78103414+raydonbabs@users.noreply.github.com>
2023-04-21 19:16:07 -0400

    Update functions

--- App/database.py
+++ App/database.py
@@ -1,13 +1,31 @@
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_migrate import Migrate
+
+# db = SQLAlchemy()
+
+# def get_migrate(app):
+#     return Migrate(app, db)
+
+# def create_db():
+#     db.create_all()
+    
+# def init_db(app):
+#     db.init_app(app)
+
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 db = SQLAlchemy()
 
+
 def get_migrate(app):
     return Migrate(app, db)
 
-def create_db():
-    db.create_all()
-    
+
+def create_db(app):
+    db.init_app(app)
+    db.create_all(app=app)
+
+
 def init_db(app):
     db.init_app(app)
