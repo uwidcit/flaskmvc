5a2e13091bdb926b5cacba85e6ba4344c73faa37
raydonbabs <78103414+raydonbabs@users.noreply.github.com>
2023-04-21 16:58:12 -0400

    Update import

--- App/views/user.py
+++ App/views/user.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, jsonify, request, send_from_directory, flash, redirect, url_for
-from flask_jwt_extended import jwt_required, current_user as jwt_current_user, current_identity as jwt_current_identity
+from flask_jwt_extended import jwt_required, current_user as jwt_current_user
 from flask_login import current_user, login_required
-#from flask_jwt import jwt_required, current_identity
+from flask_jwt import current_identity
 
 from App.controllers import(
     create_user,
