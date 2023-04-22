5711a41906e142e0d0df2054b89fc013de5bca6d
raydonbabs <78103414+raydonbabs@users.noreply.github.com>
2023-04-22 00:28:55 -0400

    Update functions

--- App/main.py
+++ App/main.py
@@ -3,40 +3,37 @@ from flask import Flask
 from flask_uploads import DOCUMENTS, IMAGES, TEXT, UploadSet, configure_uploads
 from flask_cors import CORS
 from datetime import timedelta
-import flask_excel as excel
-
-from App.database import create_db
-
-from App.controllers import setup_jwt
-
-from App.views import views
+from App.database import create_db, get_migrate
+from App.controllers import setup_jwt, load_user_from_id
+from App.views import user_views, index_views
+views = [user_views, index_views]
 
 def add_views(app):
     for view in views:
         app.register_blueprint(view)
 
-def loadConfig(app, config):
+def load_config(app, config):
     app.config["ENV"] = os.environ.get("ENV", "DEVELOPMENT")
-    delta = 7
     if app.config["ENV"] == "DEVELOPMENT":
         app.config.from_object("App.config")
-        delta = app.config["JWT_EXPIRATION_DELTA"]
     else:
-        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI")
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+            "SQLALCHEMY_DATABASE_URI"
+        )
         app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
         app.config["DEBUG"] = os.environ.get("ENV").upper() != "PRODUCTION"
         app.config["ENV"] = os.environ.get("ENV")
-        delta = os.environ.get("JWT_EXPIRATION_DELTA", 7)
-
-    app.config["JWT_EXPIRATION_DELTA"] = timedelta(days=int(delta))
-
+        app.config["JWT_EXPIRATION_DELTA"] = timedelta(
+            days=int(os.environ.get("JWT_EXPIRATION_DELTA"))
+        )
     for key, value in config.items():
         app.config[key] = config[key]
+
 
 def create_app(config={}):
     app = Flask(__name__, static_url_path="/static")
     CORS(app)
-    loadConfig(app, config)
+    load_config(app, config)
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["TEMPLATES_AUTO_RELOAD"] = True
     app.config["SEVER_NAME"] = "0.0.0.0"
@@ -44,7 +41,6 @@ def create_app(config={}):
     app.config["UPLOADED_PHOTOS_DEST"] = "App/uploads"
     photos = UploadSet("photos", TEXT + DOCUMENTS + IMAGES)
     configure_uploads(app, photos)
-    excel.init_excel(app)
     add_views(app)
     create_db(app)
     setup_jwt(app)
