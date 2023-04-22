0df5a11390cb8f7a8586e38f6d4b5471e05d1fd5
raydonbabs <78103414+raydonbabs@users.noreply.github.com>
2023-04-21 22:37:55 -0400

    Update functions

--- App/main.py
+++ App/main.py
@@ -7,7 +7,7 @@ from werkzeug.utils import secure_filename
 from werkzeug.datastructures import  FileStorage
 from datetime import timedelta
 
-from App.database import init_db
+from App.database import init_db, create_db, get_migrate_db
 from App.config import config
 
 from App.controllers import (
@@ -15,7 +15,9 @@ from App.controllers import (
     setup_flask_login
 )
 
-from App.views import views
+from App.views import(index_views, user_views)
+
+views = [index_views, user_views]
 
 def add_views(app):
     for view in views:
@@ -45,3 +47,30 @@ def create_app(config_overrides={}):
     setup_flask_login(app)
     app.app_context().push()
     return app
+
+def load_config(app, config):
+    app.config["ENV"] = os.environ.get("ENV", "DEVELOPMENT")
+    if app.config["ENV"] == "DEVELOPMENT":
+        app.config.from_object("App.config")
+    else:
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+            "SQLALCHEMY_DATABASE_URI"
+        )
+        app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+        app.config["DEBUG"] = os.environ.get("ENV").upper() != "PRODUCTION"
+        app.config["ENV"] = os.environ.get("ENV")
+        app.config["JWT_EXPIRATION_DELTA"] = timedelta(
+            days=int(os.environ.get("JWT_EXPIRATION_DELTA"))
+        )
+    for key, value in config.items():
+        app.config[key] = config[key]
+
+app = create_app()
+login_manager = LoginManager(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return load_user_from_id(user_id)
+
+
+migrate = get_migrate(app)
