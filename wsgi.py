e1f2600e859f6483d0bf8a033009c0faaed72e26
JervalThomas <jerval32@gmail.com>
2023-10-02 14:02:27 -0400

    Staff and Student Controllers

--- wsgi.py
+++ wsgi.py
@@ -4,9 +4,6 @@ from flask.cli import with_appcontext, AppGroup
 
 from App.database import db, get_migrate
 from App.main import create_app
-<<<<<<< HEAD
-from App.controllers import ( create_user, get_all_users_json, get_all_users, create_student,)
-=======
 from App.controllers import ( 
     create_user, 
     get_all_users_json, 
@@ -19,7 +16,6 @@ from App.controllers import (
     get_prerequisites,
     )
 
->>>>>>> jerrellebranch
 
 # This commands file allow you to create convenient CLI commands for testing controllers
 
