39bffcec398701ab33bdfd9bd84e3f195b7d2415
JervalThomas <jerval32@gmail.com>
2023-10-02 21:20:41 -0400

    changing controllers methods for stuuden to suit app concept

--- App/controllers/student.py
+++ App/controllers/student.py
@@ -1,17 +1,14 @@
 from App.models import Student, CoursePlan, Program
 from App.database import db
 
-def create_student(username, password, student_id, name):
-    new_student = Student(username=username, password=password, id=student_id, name=name)
+def create_student(file_path):
+    new_student = Student(file_path)
     db.session.add(new_student)
     db.session.commit()
     return new_student
 
 def get_student_by_username(username):
     return Student.query.filter_by(username=username).first()
-
-def get_student(id):
-    return Student.query.get(id)
 
 def get_all_students():
     return Student.query.all()
@@ -23,46 +20,53 @@ def get_all_students_json():
     students_json = [student.get_json() for student in students]
     return students_json
 
-def update_student(id, username):
-    student = get_student(id)
-    if student:
-        student.username = username
-        db.session.add(student)
-        db.session.commit()
-        return student
+def get_course_history(username):
+    student = get_student_by_username(username)
+    return student.course_history if student else []
 
-def enroll_in_programme(student, programme_id):
-    programme = Program.query.get(programme_id)
-    if programme:
-        student.programmes.append(programme)
-        db.session.add(student)  # Add the student object to the session
-        db.session.commit()
+def get_program(username):
+    student = get_student_by_username(username)
+    return student.program if student else ""
 
-def add_course_to_plan(student, course_id):
-    course = CoursePlan.query.get(course_id)
-    if course:
-        student.courses.append(course)
-        db.session.add(student)  # Add the student object to the session
-        db.session.commit()
+def get_nextSemCourses(username):
+    student = get_student_by_username(username)
+    return student.nextSemCourses if student else []
 
-def remove_course_from_plan(student, course_id):
-    course = CoursePlan.query.get(course_id)
-    if course:
-        student.courses.remove(course)
-        db.session.add(student)  # Add the student object to the session
-        db.session.commit()
+# def enroll_in_programme(student, programme_id):
+#     programme = Program.query.get(programme_id)
+#     if programme:
+#         student.programmes.append(programme)
+#         db.session.add(student)  # Add the student object to the session
+#         db.session.commit()
 
-def view_course_plan(student):
-    return [course.get_json() for course in student.courses]
+# def add_course_to_plan(student, course_id):
+#     #Ashely needed here 
+#     course = CoursePlan.query.get(course_id)
+#     if course:
+#         student.courses.append(course)
+#         db.session.add(student)  
+#         db.session.commit()
 
-def add_courses_from_file(student, file_path):
-    try:
-        with open(file_path, 'r') as file:
-            course_ids = [line.strip() for line in file.readlines()]
-            for course_id in course_ids:
-                add_course_to_plan(student, course_id)
-        db.session.commit()  # Commit the changes after adding courses
-    except FileNotFoundError:
-        return "File not found."
-    except Exception as e:
-        return str(e)
+# def remove_course_from_plan(student, course_id):
+#     #Ashely needed here
+#     course = CoursePlan.query.get(course_id)
+#     if course:
+#         student.courses.remove(course)
+#         db.session.add(student)  # Add the student object to the session
+#         db.session.commit()
+
+# def view_course_plan(student):
+#     #Ashely needed here
+#     return [course.get_json() for course in student.courses]
+
+# def add_courses_from_file(student, file_path):
+#     try:
+#         with open(file_path, 'r') as file:
+#             course_ids = [line.strip() for line in file.readlines()]
+#             for course_id in course_ids:
+#                 add_course_to_plan(student, course_id)
+#         db.session.commit()  # Commit the changes after adding courses
+#     except FileNotFoundError:
+#         return "File not found."
+#     except Exception as e:
+#         return str(e)
--- App/models/staff.py
+++ App/models/staff.py
@@ -1,22 +1,18 @@
-# staff.py
-
-from .user import User  # Import the User class from user.py
+from .user import User
 from App.database import db
 
 class Staff(User):
-
-    
-    id = db.Column(db.String(10), db.ForeignKey('user.id'), primary_key=True) #foreign key
+    id = db.Column(db.String(10), db.ForeignKey('user.id'), primary_key=True)
     name = db.Column(db.String(50))
 
-    def __init__(self, username, password, staff_id, name):
-        super().__init__(username, password)
+    def __init__(self, password, staff_id, name):
+        super().__init__(staff_id, password)
         self.id = staff_id
         self.name = name
 
     def get_json(self):
-        user_json = super().get_json()
-        user_json['staff_id'] = self.id
-        user_json['name'] = self.name
-        return user_json
+        return{
+            'staff_id': self.id,
+            'name': self.name,
+        }
 
