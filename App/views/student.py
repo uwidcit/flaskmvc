a21975b7c09f3c885f6929aeb1d3515d89eca231
Kyle-McIntosh <78955773+Kyle-McIntosh@users.noreply.github.com>
2023-10-08 16:44:27 +0000

    Updated student views

--- App/views/student.py
+++ App/views/student.py
@@ -14,23 +14,27 @@ from App.controllers import (
     get_student_by_id,
     get_course_by_courseCode,
     addCoursetoHistory,
-    create_easy_plan,
+    getCompletedCourseCodes,
+    generator
 )
 
 student_views = Blueprint('student_views', __name__, template_folder='../templates')
 
 ##Create student
 @student_views.route('/student', methods=['POST'])
-@jwt_required()
+#@jwt_required()
 def create_student_route():
     student_id = request.json['student_id']
     password = request.json['password']
     name = request.json['name']
     programname = request.json['programname']
 
+    if not all([student_id, password, name, programname]):
+        return jsonify({'Error': 'Missing required fields. Please provide student id, password, name, and program name.'})
+
     student = get_student_by_id(student_id)
     if student:
-        return jsonify({'error': 'Student id found'})
+        return jsonify({'Error': 'Student id found'})
     
     program = get_program_by_name(programname)
     if not program:
@@ -47,18 +51,22 @@ def add_course_to_student_route():
     course_code = request.json['course_code']
 
     if not student_id or not course_code:
-        return jsonify({'error': 'Missing required fields'})
+        return jsonify({'Error': 'Missing required fields'})
 
     # Check if the student and course exist
     student = get_student_by_id(student_id)
     course = get_course_by_courseCode(course_code)
 
     if not student:
-        return jsonify({'error': 'Student not found'})
+        return jsonify({'Error': 'Student not found'})
     if not course:
-        return jsonify({'error': 'Course not found'})
+        return jsonify({'Error': 'Course not found'})
 
-     ##need error checking for whether course already in history
+    
+    # Check if the course is already in the student's completed courses
+    completed_courses = getCompletedCourseCodes(student_id)
+    if course_code in completed_courses:
+        return jsonify({'Error': 'Course already completed'})
 
     addCoursetoHistory(student_id, course_code)
     return jsonify({'Success!': f"Course {course_code} added to student {student_id}'s course history"})
@@ -66,15 +74,20 @@ def add_course_to_student_route():
 
 ##Add course plan 
 
-@student_views.route('/student/add_easy_plan', methods=['POST'])
-def add_easy_plan():
+@student_views.route('/student/create_student_plan', methods=['POST'])
+def create_student_plan_route():
     student_id = request.json['student_id']
-    #Maybe pass in a string to determine which func to use."easy, fast, elec"
-    # Check if the student exist
+    command = request.json['command']
+    
     student = get_student_by_id(student_id)
 
     if not student:
-        return jsonify({'error': 'Student not found'})
+        return jsonify({'Error': 'Student not found'})
     
-    create_easy_plan(student)
-    return jsonify({'Success!': f"Easy plan added to student {student_id} "})
+    valid_command = ["electives", "easy", "fastest"]
+
+    if command not in valid_command:
+        return jsonify("Invalid command. Please enter 'electives', 'easy', or 'fastest'.")
+
+    courses = generator(student, command)
+    return jsonify({'Success!': f"{command} plan added to student {student_id} ", "courses" : courses})
