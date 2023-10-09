aedbb251dd02683eaa9cc580562ede9d9c36398f
Jerrelle Johnson <jerrellejohnson@Jerrelles-MacBook-Air.local>
2023-10-09 07:41:10 -0400

    added create staff to index init

--- App/views/index.py
+++ App/views/index.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, redirect, render_template, request, send_from_directory, jsonify
 from App.models import db
-from App.controllers import create_user
+from App.controllers import (create_user, create_staff)
 
 index_views = Blueprint('index_views', __name__, template_folder='../templates')
 
@@ -13,6 +13,7 @@ def init():
     db.drop_all()
     db.create_all()
     create_user('bob', 'bobpass')
+    create_staff("adminpass","999", "admin")
     return jsonify(message='db initialized!')
 
 @index_views.route('/health', methods=['GET'])
