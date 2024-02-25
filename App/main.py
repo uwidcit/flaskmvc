45c9c64867fae13b89c9acebf3c22cc8d0f95056
Nicholas Mendez <snickdx@gmail.com>
2024-02-25 15:42:45 +0000

    removed flask login references from code

--- App/main.py
+++ App/main.py
@@ -1,6 +1,5 @@
 import os
 from flask import Flask
-from flask_login import LoginManager, current_user
 from flask_uploads import DOCUMENTS, IMAGES, TEXT, UploadSet, configure_uploads
 from flask_cors import CORS
 from werkzeug.utils import secure_filename
--- App/views/user.py
+++ App/views/user.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, render_template, jsonify, request, send_from_directory, flash, redirect, url_for
 from flask_jwt_extended import jwt_required, current_user as jwt_current_user
-from flask_login import current_user, login_required
 
 from.index import index_views
 
