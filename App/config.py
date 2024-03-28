c84f0adfeffb63dc1806b8543b89457db710d022
Nicholas Mendez <snickdx@gmail.com>
2024-03-28 01:43:07 +0000

    updated config

--- App/config.py
+++ App/config.py
@@ -1,40 +1,18 @@
 import os
-import importlib.util
-import sys
 
-def load_config():
-    config = {'ENV': os.environ.get('ENV', 'DEVELOPMENT')}
-    config_base_path = './App'
-
-    # Check for custom_config.py file; adjust path as needed
-    custom_config_path = os.path.join(config_base_path, 'custom_config.py')
-    default_config_path = os.path.join(config_base_path, 'default_config.py')
-
-    if os.path.exists(custom_config_path):
-        spec = importlib.util.spec_from_file_location("custom_config", custom_config_path)
+def load_config(app, overrides):
+    if os.path.exists(os.path.join('./App', 'custom_config.py')):
+        app.config.from_object('App.custom_config')
     else:
-        spec = importlib.util.spec_from_file_location("default_config", default_config_path)
-
-    config_module = importlib.util.module_from_spec(spec)
-    sys.modules[spec.name] = config_module
-    spec.loader.exec_module(config_module)
-
-    if config['ENV'] == "DEVELOPMENT":
-        config['SQLALCHEMY_DATABASE_URI'] = config_module.SQLALCHEMY_DATABASE_URI
-        config['SECRET_KEY'] = config_module.SECRET_KEY
-        config['JWT_SECRET_KEY'] = config_module.SECRET_KEY
-    else:
-        config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI', config_module.SQLALCHEMY_DATABASE_URI)
-        config['SECRET_KEY'] = os.environ.get('SECRET_KEY', config_module.SECRET_KEY)
-        config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY', config_module.SECRET_KEY)
-        config['DEBUG'] = config['ENV'].upper() != 'PRODUCTION'
-
-    # Default configurations that don't depend on the environment
-    config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    config['TEMPLATES_AUTO_RELOAD'] = True
-    config['PREFERRED_URL_SCHEME'] = 'https'
-    config['UPLOADED_PHOTOS_DEST'] = "App/uploads"
-
-    return config
-
-config = load_config()
+        app.config.from_object('App.default_config')
+    app.config.from_prefixed_env()
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.config['PREFERRED_URL_SCHEME'] = 'https'
+    app.config['UPLOADED_PHOTOS_DEST'] = "App/uploads"
+    app.config['JWT_ACCESS_COOKIE_NAME'] = 'access_token'
+    app.config["JWT_TOKEN_LOCATION"] = ["cookies", "headers"]
+    app.config["JWT_COOKIE_SECURE"] = True
+    app.config["JWT_COOKIE_CSRF_PROTECT"] = False
+    for key in overrides:
+        app.config[key] = overrides[key]
--- App/main.py
+++ App/main.py
@@ -4,10 +4,9 @@ from flask_uploads import DOCUMENTS, IMAGES, TEXT, UploadSet, configure_uploads
 from flask_cors import CORS
 from werkzeug.utils import secure_filename
 from werkzeug.datastructures import  FileStorage
-from datetime import timedelta
 
 from App.database import init_db
-from App.config import config
+from App.config import load_config
 
 from App.controllers import (
     setup_jwt,
@@ -20,24 +19,9 @@ def add_views(app):
     for view in views:
         app.register_blueprint(view)
 
-def configure_app(app, config, overrides):
-    for key, value in config.items():
-        if key in overrides:
-            app.config[key] = overrides[key]
-        else:
-            app.config[key] = config[key]
-
-def create_app(config_overrides={}):
+def create_app(overrides={}):
     app = Flask(__name__, static_url_path='/static')
-    configure_app(app, config, config_overrides)
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['TEMPLATES_AUTO_RELOAD'] = True
-    app.config['PREFERRED_URL_SCHEME'] = 'https'
-    app.config['UPLOADED_PHOTOS_DEST'] = "App/uploads"
-    app.config['JWT_ACCESS_COOKIE_NAME'] = 'access_token'
-    app.config["JWT_TOKEN_LOCATION"] = ["cookies", "headers"]
-    app.config["JWT_COOKIE_SECURE"] = True
-    app.config["JWT_COOKIE_CSRF_PROTECT"] = False
+    load_config(app, overrides)
     CORS(app)
     add_auth_context(app)
     photos = UploadSet('photos', TEXT + DOCUMENTS + IMAGES)
