7168ced0fbfc6454674e022d23f9345b025b49fb
Zurun-Rikuthin <y.verinrii@gmail.com>
2024-04-06 14:27:21 +0000

    dynamic input creation test

--- App/views/game.py
+++ App/views/game.py
@@ -5,4 +5,8 @@ game_views = Blueprint('game_views', __name__, template_folder='../templates')
 
 @game_views.route('/game', methods=['GET'])
 def game():
-    return render_template('game.html')
+    answer_length = 5
+    return render_template('game.html', answer_length=5)
+
+def evaluateGuess():
+    pass
