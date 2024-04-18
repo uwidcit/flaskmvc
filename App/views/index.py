3418f037a680966fc05b2c2afde254f5808c0d15
Jalene Is'raell Armstrong <123189357+JaleneA@users.noreply.github.com>
2024-04-18 17:50:37 -0400

    Update index.py
    Test due to Render looking for /healthcheck

--- App/views/index.py
+++ App/views/index.py
@@ -15,6 +15,6 @@ def init():
     create_user('bob', 'bobpass')
     return jsonify(message='db initialized!')
 
-@index_views.route('/health', methods=['GET'])
+@index_views.route('/healthcheck', methods=['GET'])
 def health_check():
     return jsonify({'status':'healthy'})
