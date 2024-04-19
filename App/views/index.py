a250a11156fa5840f33fb020a6c9e89ea4dc7b0b
Nicholas Mendez <snickdx@gmail.com>
2024-04-19 12:23:50 +0000

    unified initializer

--- App/views/index.py
+++ App/views/index.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, redirect, render_template, request, send_from_directory, jsonify
-from App.models import db
-from App.controllers import create_user
+from App.controllers import create_user, initialize
 
 index_views = Blueprint('index_views', __name__, template_folder='../templates')
 
@@ -10,9 +9,7 @@ def index_page():
 
 @index_views.route('/init', methods=['GET'])
 def init():
-    db.drop_all()
-    db.create_all()
-    create_user('bob', 'bobpass')
+    initialize()
     return jsonify(message='db initialized!')
 
 @index_views.route('/health', methods=['GET'])
--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,7 @@ from flask.cli import with_appcontext, AppGroup
 
 from App.database import db, get_migrate
 from App.main import create_app
-from App.controllers import ( create_user, get_all_users_json, get_all_users )
+from App.controllers import ( create_user, get_all_users_json, get_all_users, initialize )
 
 # This commands file allow you to create convenient CLI commands for testing controllers
 
@@ -13,10 +13,8 @@ migrate = get_migrate(app)
 
 # This command creates and initializes the database
 @app.cli.command("init", help="Creates and initializes the database")
-def initialize():
-    db.drop_all()
-    db.create_all()
-    create_user('bob', 'bobpass')
+def init():
+    initialize()
     print('database intialized')
 
 '''
