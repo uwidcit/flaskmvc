25c5fdd09f853ff25d3af941bb06f44d84164167
eddie909 <mikey123044@gmail.com>
2024-04-20 16:56:14 +0000

    ignore

--- App/views/testing.py
+++ App/views/testing.py
@@ -19,7 +19,7 @@ def fetch_data(param):
     'X-RapidAPI-Host': "work-out-api1.p.rapidapi.com"
 }
 
-    conn.request("GET", f"/search?Muscles={param}", headers=headers)
+    conn.request("GET", f"/search?Muscles={param}&Equipment=Barbell", headers=headers)
 
     res = conn.getresponse()
     data = res.read()
@@ -27,12 +27,37 @@ def fetch_data(param):
     #print(data.decode("utf-8"))
     return data.decode('utf-8')
 
+def fetch_content(type,param):
+    load_dotenv()
+    api_key=os.getenv("API_KEY")
+
+    conn = http.client.HTTPSConnection("work-out-api1.p.rapidapi.com")
+
+    headers = {
+    'X-RapidAPI-Key': api_key,
+    'X-RapidAPI-Host': "work-out-api1.p.rapidapi.com"
+}
+    conn.request("GET", f"/search?{type}={param}", headers=headers)
+
+    res = conn.getresponse()
+    data = res.read()
+
+    return data.decode('utf-8')
+
 @testing_views.route('/testing', methods=['GET'])
 def testing_page():
-    app_data = fetch_data("biceps")
-    app_data=json.loads(app_data)
-    print (len(app_data))
-    first=app_data[1]
-    muscle=first.get('WorkOut')
-    return render_template('testing.html', api_data=muscle)
+    # app_data = fetch_data("Chest")
+    # app_data=json.loads(app_data)
+    # print (len(app_data))
+    # first=app_data[0]
+    # muscle=first.get('WorkOut')
+
+    data=fetch_content('Intensity_Level','Beginner')
+    data=json.loads(data)
+    arr=[]
+    for item in data:
+        work=item.get('WorkOut')
+        arr.append(work)
+    print(f'Length: {len(arr)}')
+    return render_template('testing.html', api_data=arr)
 
