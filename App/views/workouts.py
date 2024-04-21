0468e344506754ac9dce5ac7767b3906d6435d4e
SGajadhar <shaniajgajadhar@gmail.com>
2024-04-21 19:45:13 +0000

    workout_pages

--- App/views/workouts.py
+++ App/views/workouts.py
@@ -1,10 +1,12 @@
-from flask import Blueprint, redirect, render_template, request, send_from_directory,jsonify,url_for
+from flask import Blueprint, redirect, render_template, request, send_from_directory,jsonify,url_for,flash
 from App.models import db
 from App.controllers.auth import jwt_required
 from App.controllers.workout import *
+from App.controllers.userWorkouts import *
+from App.controllers.routine import *
 import json
-from App.controllers.auth import jwt_required
-from App.controllers.workout import *
+
+
 
 workout_views= Blueprint('workout_views', __name__, template_folder='../templates')
 
@@ -12,19 +14,21 @@ workout_views= Blueprint('workout_views', __name__, template_folder='../template
 @workout_views.route('/workout/<data>', methods=['GET'])
 def work_page(data):
     workouts=get_workout_by_type(data)
+    routines=get_all_routines()
 
 
-
-    return render_template('workouts.html',workouts=workouts)
+    return render_template('workouts.html',workouts=workouts,routines=routines)
 
 
 @workout_views.route('/save_workout', methods=['POST'])
 def save_workout():
     data=request.form
     
+    workout=addToRoutine(data['routine_id'],data['workout_id'])
+    flash(f'{data["muscle_type"]}: {data["workout"]} saved')
+    return redirect(request.referrer)
 
-    flash(f'{data["Muscles"]} workout saved')
-    return render_template(request.referrer)
+
 
 @workout_views.route('/remove_workout', methods=['DELETE'])
 @jwt_required()
