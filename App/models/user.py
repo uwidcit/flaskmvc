0e2ba1f6bb749dc92830873ae318130a8d1ed01b
Nicholas Mendez <snickdx@gmail.com>
2024-09-19 21:59:46 +0000

    updated user model

--- App/models/user.py
+++ App/models/user.py
@@ -3,7 +3,7 @@ from App.database import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username =  db.Column(db.String, nullable=False, unique=True)
+    username =  db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(120), nullable=False)
 
     def __init__(self, username, password):
