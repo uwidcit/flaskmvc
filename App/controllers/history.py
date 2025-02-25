10e87e958bbb8d79ce84febfea1f066a43b10ae1
PhineasMun <123189426+PhineasMun@users.noreply.github.com>
2025-02-25 00:56:25 +0000

    history-1

--- App/controllers/history.py
+++ App/controllers/history.py
@@ -1,5 +1,15 @@
 from App.models import History
 from App.database import db
 from App.models import Asset 
+from App.controllers import Asset
 
 #def get_missing():
+
+def get_all_assets_by_condition(condition):
+    return Asset.query.filter_by(condition = condition).all()
+
+def get_all_assets_by_assignee(id):
+    return Asset.query.filter_by(id = id).all()
+
+def get_all_assets_by_location(id):
+    return Asset.query.filter_by(id = id).all()
