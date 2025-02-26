8d99a4cb3c25e1a3cd828e01af380c47b5bdabd8
Enzo-TTD <lorenzogouldd@gmail.com>
2025-02-26 05:11:08 +0000

    added building model and controller

---
+++ App/models/building.py
@@ -0,0 +1,15 @@
+from App.database import db
+
+class Building(db.Model):
+    building_id = db.Column(db.String(30), primary_key=True)
+    building_name = db.Column(db.String(30), nullable=False)
+    
+def __init__ (self, building_id, building_name):
+    self.building_id = building_id
+    self. building_name = building_name
+    
+def get_json(self):
+    return{
+        'building_id':self.building_id,
+        'building_name':self.building_name
+    }
