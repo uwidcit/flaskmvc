849aa319b9b53758c6eadfd87514785388bd0c32
PhineasMun <123189426+PhineasMun@users.noreply.github.com>
2025-02-26 14:06:20 +0000

    imports all relevant controllers in wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -4,6 +4,15 @@ from flask.cli import with_appcontext, AppGroup
 
 from App.database import db, get_migrate
 from App.models import User
+from App.controllers.asset import *
+from App.controllers.assetassignment import *
+from App.controllers.assignee import *
+from App.controllers.building import *
+from App.controllers.floor import *
+from App.controllers.history import *
+from App.controllers.provider import *
+from App.controllers.room import *
+from App.controllers.scanevent import *
 from App.main import create_app
 from App.controllers import ( create_user, get_all_users_json, get_all_users, initialize )
 
