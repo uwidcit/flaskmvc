0899c6f3f6183a801710c93bba7fbdcc58383a98
PhineasMun <123189426+PhineasMun@users.noreply.github.com>
2025-02-26 04:24:03 +0000

    names and attributes of 'scanevent' model updated to better reflect model diagram

--- App/models/scanevent.py
+++ App/models/scanevent.py
@@ -4,23 +4,23 @@ from App.models import Location
 from App.models import Assignee
 from App.models import Asset
 
-class Scanevent(db.Model):
+class ScanEvent(db.Model):
     scan_id = db.Column(db.String, primary_key = True, nullable = False, unique = True)
     asset_id = db.Column(db.String, db.ForeignKey('asset.id'), nullable = False)
     user_id = db.Column(db.String, db.ForeignKey('user.id'), nullable = False)
-    location_id = db.Column(db.String, db.ForeignKey('location.id'), nullable = False)
+    room_id = db.Column(db.String, db.ForeignKey('location.id'), nullable = False)
+    scan_time = db.Column(db.DateTime, default=db.func.current_timestamp())
     status = db.Column(db.String, Nullable = False)
-    last_update = db.Column(db.DateTime, default=db.func.current_timestamp())
-    change_log = db.Column(db.String(120), Nullable = True)
+    notes = db.Column(db.String(120), Nullable = True)
     
-def __init__(self, scan_id, asset_id, user_id, location_id, status, last_update, change_log):
+def __init__(self, scan_id, asset_id, user_id, room_id, scan_time, status, notes):
     self.scan_id = scan_id
     self.asset_id = asset_id
     self.user_id = user_id
-    self.location_id = location_id
+    self.room_id = room_id
+    self.scan_time = scan_time
     self.status = status
-    self. last_update = last_update
-    self.change_log = change_log
+    self.notes = notes
 
 
 def get_json(self):
@@ -28,8 +28,8 @@ def get_json(self):
         'scan_id: ': self.scan_id,
         'asset_id: ': self.asset_id,
         'user_id: ': self.user_id,
-        'location_id: ': self.location_id,
+        'room_id: ': self.room_id,
+        'scan_time: ': self.scan_time,
         'status: ': self.status,
-        'last_update: ': self.last_update,
-        'change_log: ': self.change_log
+        'notes: ': self.notes
     }
