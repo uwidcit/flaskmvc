cfd3e181ffac56a0002d675a6e822681242d8211
Analisa008 <125392976+AnalisaMohamed@users.noreply.github.com>
2025-03-07 19:49:57 +0000

    Merged history and scanevent

--- App/models/scanevent.py
+++ App/models/scanevent.py
@@ -2,6 +2,7 @@ from App.database import db
 from sqlalchemy import *
 from App.models import Assignee
 from App.models import Asset
+from datetime import datetime
 
 class ScanEvent(db.Model):
     scan_id = db.Column(db.String, primary_key = True, nullable = False, unique = True)
@@ -11,8 +12,13 @@ class ScanEvent(db.Model):
     scan_time = db.Column(db.DateTime, default=db.func.current_timestamp())
     status = db.Column(db.String, nullable = False)
     notes = db.Column(db.String(120), nullable = True)
+    id = db.Column(db.Integer, primary_key = True, nullable = False, unique = True)
+    asset = db.relationship('Asset', back_populates='history', overlaps="asset")
+    last_update = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    changeLog = db.Column(db.String(200), nullable = True)
     
-    def __init__(self, scan_id, asset_id, user_id, room_id, scan_time, status, notes):
+    
+    def __init__(self, scan_id, asset_id, user_id, room_id, scan_time, status, notes, last_update, changeLog):
         self.scan_id = scan_id
         self.asset_id = asset_id
         self.user_id = user_id
@@ -20,6 +26,8 @@ class ScanEvent(db.Model):
         self.scan_time = scan_time
         self.status = status
         self.notes = notes
+        self.last_update = last_update
+        self.changeLog = changeLog
 
 
     def get_json(self):
@@ -30,5 +38,8 @@ class ScanEvent(db.Model):
             'room_id: ': self.room_id,
             'scan_time: ': self.scan_time,
             'status: ': self.status,
-            'notes: ': self.notes
+            'notes: ': self.notes,
+            'last_update: ': self.last_update,
+            'change log: ': self.changeLog
         }
+    
