894dd0e13265478eec28c4a69dd73b2c0a94fed0
Analisa008 <125392976+AnalisaMohamed@users.noreply.github.com>
2025-03-08 19:04:56 +0000

    Integration tests for location

--- App/tests/test_app.py
+++ App/tests/test_app.py
@@ -2,7 +2,11 @@ import os, tempfile, pytest, logging, unittest
 from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
 from App.models import Building, Floor, Room
-from App.controllers import building, floor, room
+from App.controllers import (
+    create_building, get_building, 
+    create_floor, get_floor,
+    create_room, get_room, 
+)
 from App.main import create_app
 from App.database import db, create_db
 from App.models import User
@@ -127,3 +131,41 @@ class UsersIntegrationTests(unittest.TestCase):
         user = get_user(1)
         assert user.username == "ronnie"
         
+class BuildingIntegrationTests (unittest.TestCase):
+
+    def test_create_building(self):
+        building = create_building("B3", "Third building")
+        self.assertEqual(building.building_name, "Third building")
+
+    def test_get_building(self):
+        create_building("B1", "Main Building")
+        building = get_building("B1")  
+        self.assertIsNotNone(building)
+        self.assertEqual(building.building_name, "Main Building")
+
+
+    
+
+class FloorIntegrationTests(unittest.TestCase):
+
+    def test_create_floor(self):
+        floor = create_floor("F3", "B3", "Third Floor")
+        self.assertEqual(floor.floor_name, "Third Floor")
+
+    def test_get_floor(self):
+        create_floor("F4", "B3", "Fourth Floor")
+        floor = get_floor("F4")
+        self.assertIsNotNone(floor)
+        self.assertEqual(floor.floor_name, "Fourth Floor")
+
+class RoomIntegrationTests(unittest.TestCase):
+
+    def test_create_room(self):
+        room = create_room("R3", "F3", "Room 3")
+        self.assertEqual(room.room_name, "Room 3")
+
+    def test_get_room(self):
+        create_room("R4", "F3", "Room 4")
+        room = get_room("R4")
+        self.assertIsNotNone(room)
+        self.assertEqual(room.room_name, "Room 4")
