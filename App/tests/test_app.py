05bb6e26633cb3ca5537478004d54f0bbbf2d4c9
Analisa008 <125392976+AnalisaMohamed@users.noreply.github.com>
2025-03-18 01:20:53 +0000

    unit test for assetassignment

--- App/tests/test_app.py
+++ App/tests/test_app.py
@@ -127,6 +127,34 @@ class AssigneeUnitTests(unittest.TestCase):
         }
         self.assertEqual(a.get_json(), expected)
 
+class AssetAssignmentUnitTests(unittest.TestCase):
+
+    def test_new_asset_assignment(self):
+        aa = AssetAssignment(
+            "AA1", "asset1", "assigned1", "F1",
+            assignment_date="2025-03-07 12:00:00", return_date="2025-03-08 12:00:00"
+        )
+        self.assertEqual(aa.assignment_id, "AA1")
+        self.assertEqual(aa.asset_id, "asset1")
+        self.assertEqual(aa.assigned_to_assignee_id, "assigned1")
+        self.assertEqual(aa.floor_id, "F1")
+        self.assertEqual(aa.assignment_date, "2025-03-07 12:00:00")
+        self.assertEqual(aa.return_date, "2025-03-08 12:00:00")
+
+    def test_get_json(self):
+        aa = AssetAssignment(
+            "AA1", "asset1", "assignee1", "1st Floor",
+            assignment_date="2025-03-07 12:00:00", return_date="2025-03-08 12:00:00"
+        )
+        expected = {
+            'assignment_id': "AA1",
+            'asset_id': "asset1",
+            'assigned_to_assignee_id': "assignee1",
+            'floor_id': "1st Floor",
+            'assignment_date': "2025-03-07 12:00:00",
+            'return_date': "2025-03-08 12:00:00"
+        }
+        self.assertEqual(aa.get_json(), expected)
 
 '''
     Integration Tests
