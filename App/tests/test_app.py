dcc10106bf7f4f8e722231875e1a99539ecbba9b
PhineasMun <123189426+PhineasMun@users.noreply.github.com>
2025-03-21 22:56:35 +0000

    scan event unit tests created

--- App/tests/test_app.py
+++ App/tests/test_app.py
@@ -144,32 +144,32 @@ class AssetUnitTests(unittest.TestCase):
         }
         self.assertDictEqual(asset.get_json(), expected_json)
         
-# class ScanEventUnitTest(unittest.TestCase):
-#     def test_new_scanevent(self):
-#         scanevent = ScanEvent("01", "01", "01", "01", "30-12-2024", "Good", "scanned successfully", "15-09-2024", "Original owner")
-#         self.assertEqual(scanevent.asset_id, "01")
-#         self.assertEqual(scanevent.user_id, "01")
-#         self.assertEqual(scanevent.room_id, "01")
-#         self.assertEqual(scanevent.scan_time, "30-12-2024")
-#         self.assertEqual(scanevent.status, "Good")
-#         self.assertEqual(scanevent.notes, "scanned successfully")
-#         self.assertEqual(scanevent.last_update, "15-09-2024")
-#         self.assertEqual(scanevent.changeLog, "Original owner")
+class ScanEventUnitTest(unittest.TestCase):
+    def test_new_scanevent(self):
+        scanevent = ScanEvent("01", "01", "01", "01", "30-12-2024", "Good", "scanned successfully", "15-09-2024", "Original owner")
+        self.assertEqual(scanevent.asset_id, "01")
+        self.assertEqual(scanevent.user_id, "01")
+        self.assertEqual(scanevent.room_id, "01")
+        self.assertEqual(scanevent.scan_time, "30-12-2024")
+        self.assertEqual(scanevent.status, "Good")
+        self.assertEqual(scanevent.notes, "scanned successfully")
+        self.assertEqual(scanevent.last_update, "15-09-2024")
+        self.assertEqual(scanevent.changeLog, "Original owner")
         
-#     def test_get_json(self):
-#         scanevent = ScanEvent("01", "02", "03", "04", "30-12-2024", "Good", "scanned successfully", "15-09-2024", "Original owner")
-#         expected_json = {
-#             'scan_id: ': "01",
-#             'asset_id: ': "02",
-#             'user_id: ': "03",
-#             'room_id: ': "04",
-#             'scan_time: ': "30-12-2024",
-#             'status: ': "Good",
-#             'notes: ': "scanned successfully",
-#             'last_update: ': "15-09-2024",
-#             'change log: ': "Original owner"
-#         }
-#         self.assertDictEqual(scanevent.get_json(), expected_json)
+    def test_get_json(self):
+        scanevent = ScanEvent("01", "02", "03", "04", "30-12-2024", "Good", "scanned successfully", "15-09-2024", "Original owner")
+        expected_json = {
+            'scan_id: ': "01",
+            'asset_id: ': "02",
+            'user_id: ': "03",
+            'room_id: ': "04",
+            'scan_time: ': "30-12-2024",
+            'status: ': "Good",
+            'notes: ': "scanned successfully",
+            'last_update: ': "15-09-2024",
+            'change log: ': "Original owner"
+        }
+        self.assertDictEqual(scanevent.get_json(), expected_json)
         
         
 '''
