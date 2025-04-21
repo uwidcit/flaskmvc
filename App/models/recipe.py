f086ae93897564b98583d15439bab335f49b4827
AnuRam03 <anuradha.ramlakhan03@gmail.com>
2025-04-20 22:17:36 -0400

    Update recipe.py

--- App/models/recipe.py
+++ App/models/recipe.py
@@ -6,7 +6,8 @@ class Recipe(db.Model):
     directions = db.Column(db.String(1000))
     total_time_taken = db.Column(db.Integer)
     #user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False) #not using it like this as the todo list allocation is predefined
-    ingredients = db.relationship('Ingredient', backref='recipe', cascade="all, delete-orphan")
+    #ingredients = db.relationship('Ingredient', backref='recipe', cascade="all, delete-orphan")
+    recipe_ingredient = db.relationship('RecipeIngredient', backref='recipe', lazy=True)
     user = db.relationship('UserRecipe', backref='recipe', lazy=True)
 
     def __init__(self, name, directions, time):
