1276aa1dc21e4a3bdba6ef206ce591b7da5a450f
AnuRam03 <anuradha.ramlakhan03@gmail.com>
2025-04-20 22:18:20 -0400

    Update inventory.py

--- App/models/inventory.py
+++ App/models/inventory.py
@@ -4,14 +4,17 @@ class Inventory(db.Model):
     inventory_id = db.Column(db.Integer, primary_key=True) #thinking of making this equal to the user id cause, each user only evar has 1 id
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
     ingredient_id = db.Column(db.Integer, db.ForeignKey('ingredient.ingredient_id'), nullable = False)
+    amount = db.Column(db.Integer)
 
-    def __init__(self, user_id, ingredient_id):
+    def __init__(self, user_id, ingredient_id, amount):
         self.user_id = user_id
         self.ingredient_id = ingredient_id
+        self.amount = amount
 
     def get_json(self):
         return{
             "inventory_id": self.inventory_id,
             "user_id": self.user_id,
-            "ingredient_id": self.ingredient_id
+            "ingredient_id": self.ingredient_id,
+            "amount": self.amount
         }
