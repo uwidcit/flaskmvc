5f098729f043260a85d8b61b7976f21c46214b55
AnuRam03 <anuradha.ramlakhan03@gmail.com>
2025-04-20 22:23:52 -0400

    Update ingredient.py

--- App/models/ingredient.py
+++ App/models/ingredient.py
@@ -3,11 +3,10 @@ from App.database import db
 class Ingredient(db.Model):
     ingredient_id = db.Column(db.Integer, primary_key=True)
     ingredient_name = db.Column(db.String(225), nullable=False)
-    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.recipe_id'), nullable = False)
+    ingredient_recipe = db.relationship('RecipeIngredient', backref='ingredient', lazy=True)
 
-    def __init__(self, name, recipe_id):
+    def __init__(self, name):
         self.ingredient_name = name
-        self.recipe_id = recipe_id
 
     def get_json(self):
         return{
