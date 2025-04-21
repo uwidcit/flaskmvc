d8757fe7e851876e682897894df62056cc5ec47b
AnuRam03 <anuradha.ramlakhan03@gmail.com>
2025-04-20 22:26:05 -0400

    Update user.py

--- App/models/user.py
+++ App/models/user.py
@@ -5,6 +5,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username =  db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(120), nullable=False)
+    inventory_ingredients = db.relationship('Inventory', backref='user', cascade="all, delete-orphan")
+    recipes = db.relationship('UserRecipe', backref='user', lazy=True)
 
     def __init__(self, username, password):
         self.username = username
@@ -23,4 +25,3 @@ class User(db.Model):
     def check_password(self, password):
         """Check hashed password."""
         return check_password_hash(self.password, password)
-
